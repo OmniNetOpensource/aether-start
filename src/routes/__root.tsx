e43a2a5f5579de9080f837719e632b998de0bf68
jiuzhe666 <skylerloveuk@gmail.com>
2026-04-08 18:36:57 +0800

    fix(routes): update font display setting for improved performance
    - Changed the font display setting in the RootDocument component from 'swap' to 'optional' to enhance loading performance and reduce layout shifts.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -81,7 +81,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='' />
         <link
           rel='stylesheet'
-          href='https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@400;500;600;700&display=swap'
+          href='https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@400;500;600;700&display=optional'
         />
         <link rel='stylesheet' href={appCss} />
         <link rel='manifest' href='/manifest.webmanifest' />
